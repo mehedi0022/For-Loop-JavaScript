3ae9093399dee89ade13a9d5339068711077cb41
mehedi0022 <mdmehedihassan0022@gmail.com>
2022-04-03 19:44:49 +0600

    Fixed

--- app.js
+++ app.js
@@ -2,8 +2,15 @@
 let startingNumber = parseInt(prompt("Enter Starting Number: "));
 let lastNumber = parseInt(prompt("Enter Last Number: "));
 let sum=0;
+let count;
 for(startingNumber; startingNumber<=lastNumber; startingNumber++){
     sum = sum + startingNumber;
-    document.write(startingNumber + "+");
+    document.write(startingNumber);
+
+    count = startingNumber;
+    if(count < lastNumber){
+        document.write("+");
+        count++
+    }
 }
 document.write(" = "+ sum);
